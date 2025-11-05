222bd9e63794c182e396854515723d0c15eab444
dkobylianskii <kobad2@gmail.com>
2025-11-05 15:50:18 +0200

    Enchance VarTransform + add Unscaler class Update typings

--- src/parnassus/utils/typing.py
+++ src/parnassus/utils/typing.py
@@ -1,9 +1,13 @@
 import numpy as np
 import numpy.typing as npt
+from torch import Tensor
+
+type TensorDict = dict[str, Tensor]
 
 type FloatArray = npt.NDArray[np.float32]
 type IntArray = npt.NDArray[np.int32]
 type LongArray = npt.NDArray[np.int64]
 type BoolArray = npt.NDArray[np.bool]
+type VarNameTuple = tuple[str, ...]
 
-__all__ = ["BoolArray", "FloatArray", "IntArray", "LongArray"]
+__all__ = ["BoolArray", "FloatArray", "IntArray", "LongArray", "TensorDict", "VarNameTuple"]
