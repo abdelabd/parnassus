569eff78e3f9beccbce6c175330ef60769cee98d
dkobylianskii <kobad2@gmail.com>
2025-11-13 19:49:42 +0200

    Fix typing

--- src/parnassus/utils/typing.py
+++ src/parnassus/utils/typing.py
@@ -7,7 +7,7 @@ type TensorDict = dict[str, Tensor]
 type FloatArray = npt.NDArray[np.float32]
 type IntArray = npt.NDArray[np.int32]
 type LongArray = npt.NDArray[np.int64]
-type BoolArray = npt.NDArray[np.bool]
+type BoolArray = npt.NDArray[np.bool_]
 type VarNameTuple = tuple[str, ...]
 
 __all__ = ["BoolArray", "FloatArray", "IntArray", "LongArray", "TensorDict", "VarNameTuple"]
