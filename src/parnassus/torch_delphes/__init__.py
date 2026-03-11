76e54fb139fb3f4b13886803ea7a08a13c67f789
Abdelrahman Elabd <aelabd@login18.chn.perlmutter.nersc.gov>
2026-03-10 20:14:21 -0700

    update default-module location

--- src/parnassus/torch_delphes/__init__.py
+++ src/parnassus/torch_delphes/__init__.py
@@ -4,9 +4,7 @@ from .Merger import Merger
 from .MomentumSmearing import MomentumSmearing
 from .ParticlePropagator import ParticlePropagator
 from .SimpleCalorimeter import SimpleCalorimeter
-from .CMSDefault import CMSEnergyFlowDefault
-from .ATLASDefault import ATLASEnergyFlowDefault
+
 
 __all__ = ["Efficiency", "EFlowMerger", "Merger", "MomentumSmearing", 
-           "ParticlePropagator", "SimpleCalorimeter", "CMSEnergyFlowDefault",
-           "ATLASEnergyFlowDefault"]
+           "ParticlePropagator", "SimpleCalorimeter",]
---
+++ src/parnassus/torch_delphes/defaults/__init__.py
@@ -0,0 +1,4 @@
+from .CMSDefault import CMSEnergyFlowDefault
+from .ATLASDefault import ATLASEnergyFlowDefault
+
+__all__ = ["CMSEnergyFlowDefault", "ATLASEnergyFlowDefault"]
